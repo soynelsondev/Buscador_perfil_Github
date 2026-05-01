50b0e9a332c612b0aea8ee2aec95170b9c6e98ae
Nelson dev <soynelsondev@gmail.com>
2026-04-30 23:45:34 -0400

    update

--- app/src/main/java/com/example/buscador_github/data/BuscadorResponse.kt
+++ app/src/main/java/com/example/buscador_github/data/BuscadorResponse.kt
@@ -1,2 +1,22 @@
 package com.example.buscador_github.data
 
+import com.google.gson.annotations.SerializedName
+
+
+data class GithubUsers(
+    @SerializedName("login")
+    val nombreUsuario : String,
+
+    @SerializedName("avatar_url")
+    val fotoPerfil : String,
+
+    @SerializedName("html_url")
+     val urlPerfil : String
+
+
+)
+
+data  class BuscarResponse(
+    @SerializedName("items")
+    val listaUsuarios: List<GithubUsers>
+)
