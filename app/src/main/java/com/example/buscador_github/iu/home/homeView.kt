946dab7eafc6f3803eac0fc7a3b7d03f4a2fb04f
Nelson dev <soynelsondev@gmail.com>
2026-05-01 10:23:36 -0400

    subir

--- app/src/main/java/com/example/buscador_github/iu/home/homeView.kt
+++ app/src/main/java/com/example/buscador_github/iu/home/homeView.kt
@@ -1,2 +1,36 @@
 package com.example.buscador_github.iu.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.buscador_github.ui.theme.FondoCard
+
+
+@Composable
+fun Principal(viewModel: BucadorViewModel){
+
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun tarjetaUser(){
+
+    Card ( shape=RoundedCornerShape(12.dp) ,
+            modifier = Modifier.fillMaxWidth() .height(130.dp),
+        colors = CardDefaults.cardColors(containerColor = FondoCard)){
+
+        Column(modifier = Modifier.fillMaxSize() .padding(4.dp) ) {
+
+        }
+    }
+}
