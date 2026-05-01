099bed391af33d9eb4ff0af8ac436058adae1533
Nelson dev <soynelsondev@gmail.com>
2026-05-01 00:31:47 -0400

    retrofit

--- app/src/main/java/com/example/buscador_github/data/RetrofitClient.kt
+++ app/src/main/java/com/example/buscador_github/data/RetrofitClient.kt
@@ -1,6 +1,17 @@
 package com.example.buscador_github.data
 
-object RetrofitClient {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
-    
+object RetrofitClient {
+    private const val BaseUrl ="https://api.github.com/"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val apiService: ApiBuscador = retrofit.create(ApiBuscador::class.java)
+
 }
